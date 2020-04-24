6555ed57c9b39716c0b3eb5e0fc06b47f1c93cea
Nick Riabovol <nick.riabovol@gmail.com>
2020-04-24 20:56:51 +0300

    Changed FastDateFormat to SimpleDateFormat

--- services/src/main/java/org/exoplatform/outlook/web/RequestUtils.java
+++ services/src/main/java/org/exoplatform/outlook/web/RequestUtils.java
@@ -19,11 +19,11 @@
  */
 package org.exoplatform.outlook.web;
 
-import org.apache.commons.lang3.time.FastDateFormat;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
@@ -36,12 +36,14 @@ import java.util.TimeZone;
  */
 public class RequestUtils {
 
-  /** The Constant EXPIRES_DATE_FORMAT. */
-  public static final FastDateFormat EXPIRES_DATE_FORMAT            = FastDateFormat.getInstance("EEE, dd MMM yyyy HH:mm:ss zzz",
-                                                                                                 TimeZone.getTimeZone("GMT"));
+  /** The Constant DATE_FORMAT. */
+  public static final String DATE_FORMAT                    = "EEE, dd MMM yyyy HH:mm:ss zzz";
+
+  /** The Constant TIME_ZONE. */
+  public static final String TIME_ZONE                      = "GMT";
 
   /** The Constant SAME_SITE_NONE_ATTRIBUTE_VALUE */
-  public static final String         SAME_SITE_NONE_ATTRIBUTE_VALUE = "None";
+  public static final String SAME_SITE_NONE_ATTRIBUTE_VALUE = "None";
 
   /**
    * Instantiates a new request utils.
@@ -112,7 +114,10 @@ public class RequestUtils {
     expireDate.setTime(new Date());
     expireDate.add(Calendar.SECOND, maxAge);
 
-    return EXPIRES_DATE_FORMAT.format(expireDate);
+    DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+    dateFormat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
+
+    return dateFormat.format(expireDate.getTime());
   }
 
   private static void appendToCookie(StringBuilder cookie, String key, String value) {
